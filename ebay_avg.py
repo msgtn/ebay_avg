01b8084b8ec4821aedb5202c29e745739de9cb81
Michael Suguitan <psychomugs@gmail.com>
2016-08-17 16:28:25 -0400

    First working

---
+++ ebay_avg.py
@@ -0,0 +1,16 @@
+from bs4 import BeautifulSoup as bs
+import urllib2
+hdr = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11','Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8','Accept-Charset': 'ISO-8859-1,utf-8;q=0.7,*;q=0.3','Accept-Encoding': 'none','Accept-Language': 'en-US,en;q=0.8','Connection': 'keep-alive'}
+
+search_term = raw_input('Search term: ')
+url = 'http://www.ebay.com/sch/i.html?_nkw=' + search_term.replace(' ', '+')
+if (raw_input('Sold only (y/n) ? ') == 'y'):
+    url = url + '&LH_Complete=1&LH_Sold=1'
+
+req = urllib2.Request(url, headers=hdr)
+page = urllib2.urlopen(req)
+soup = bs(page.read(), 'html.parser')
+price_raw = soup.find_all('span', {'class':'bold bidsold'})
+price_list = []
+for p in price_raw:
+    price_list.append(float(p.contents[0].replace('$','').replace(',','')))
